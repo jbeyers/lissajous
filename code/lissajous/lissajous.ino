0c1d72f7ceb701b35b530ce1380c0afd17824908
Johan Beyers <johan@juizi.com>
2013-07-22 09:35:20 +0200

    Major commenting effort

--- code/lissajous/lissajous.ino
+++ code/lissajous/lissajous.ino
@@ -1,46 +1,58 @@
-// Sweep
+// This is based on Sweep
 // by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
 
 
 #include <Servo.h> 
  
-Servo servox;  // create servo object to control a servo 
-Servo servoy; // a maximum of eight servo objects can be created 
+// Create the servo objects
+Servo servox;
+Servo servoy;
  
-double pos = 0;    // variable to store the servo position 
-float spd = 0.005;
-float facter = 1.0;
-float starter = 0;
-int adjx = 90;
-int adjy = 90;
-float decay = 60000;
+double pos = 0; // Position incrementer
+float spd = 0.005; // This value seems to work for reasonable speed.
+float facter = 1.0; // Ratio between servox and servoy periods.
+float starter = 0; // Starting position offset between the servos.
+int adjx = 90; // Position of servox
+int adjy = 90; // Position of servoy
+float decay = 60000; // Gives the amount of steps to run on. Higher gives a slower decay.
  
 void setup() 
 { 
   servox.attach(9);  // attaches the servo on pin 9 to the servo object 
   servoy.attach(10);  // attaches the servo on pin 10 to the servo object 
-  pinMode(2, INPUT_PULLUP);
-  randomSeed(analogRead(0));
+  pinMode(2, INPUT_PULLUP); // Starting button input
+  randomSeed(analogRead(0)); // Just seed with a random value
 } 
  
  
 void loop() 
 { 
+
+  // Wait for a button press.
   while (digitalRead(2) != 0)
   {
-    delay(10);
-    facter = random(2001, 2021)/1000.0;
+    // Keep generating random values. Only on button press do we lock in the values.
+    facter = random(1001, 1021)/1000.0;
     starter = random(3141)/1000.0;
+    delay(10);
   }
-  for(pos = 0; pos < decay*0.9; pos += 1) 
+
+  // Main loop, to 90% decay.
+  for(pos = 0; pos < decay*0.9; pos += 1)
   {  
+    // Adjx just gives me a nice sine wave
     adjx = int(90 + int(70 * (1 - pos/decay) * sin(pos * spd)));
+
+    // Also a sinewave, with offsets and different period.
     adjy = int(90 + int(70 * (1 - pos/decay) * sin(starter + (pos * spd * facter))));
+
+    // Get the values to the servos.
     servox.write(adjx);
     servoy.write(adjy);
-    delay(1);                       // waits 15ms for the servo to reach the position 
+    delay(1); // Just for consistent speed.
   } 
+
+  // Return to center after drawing.
   servox.write(90);
   servoy.write(90);
 }
