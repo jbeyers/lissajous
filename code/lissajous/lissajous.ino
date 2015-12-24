78b67f6b270efcab079e3d93d8a68f10fa94a904
Johan Beyers <johan@juizi.com>
2015-12-24 08:11:12 +0200

    Scale up the image to max servo range

--- code/lissajous/lissajous.ino
+++ code/lissajous/lissajous.ino
@@ -17,6 +17,9 @@ float starter2 = 0; // Starting position offset between the servos.
 int adjx = 90; // Position of servox
 int adjy = 90; // Position of servoy
 float decay = 60000; // Gives the amount of steps to run on. Higher gives a slower decay.
+int scale = 70; // Scaling the image to maximum size. Gets adjusted after first run.
+int max_dev = 0; // Keeping track of maximum deviation of the servos.
+int counter = 0; // How many times through the main loop.
 
 void setup() 
 { 
@@ -40,22 +43,39 @@ void loop()
         decay = 100000 + random(100000);
     }
     // Main loop, to 90% decay in amplitude of the movements.
-    for(pos = int(decay*0.1); pos < decay; pos += 1)
-    {  
-        // Adjx just gives me a nice sine wave
-        adjx = int(90 + int(35 * pos/decay * sin(pos * spd)));
+    
+    counter = 0;
+    max_dev = 0;
+    scale = 70;
+    while (counter < 2)
+    {
+        for(pos = int(decay*0.1); pos < decay; pos += 1)
+        {  
+            // Adjx just gives me a nice sine wave
+            adjx = int(90 + int(scale * pos/decay * sin(pos * spd)));
 
-        // Also a sinewave, with offsets and different period.
-        adjy = int(90 + int(35 * (pos/decay) * sin(starter + (pos * spd * facter))));
+            // Also a sinewave, with offsets and different period.
+            adjy = int(90 + int(scale * (pos/decay) * sin(starter + (pos * spd * facter))));
 
-        // Add a circular motion.
-        adjx += int(35 * (pos/decay) * sin(starter2 + (pos * spd * facter2)));
-        adjy += int(35 * (pos/decay) * cos(starter2 + (pos * spd * facter2)));
+            // Add a circular motion.
+            adjx += int(scale * (pos/decay) * sin(starter2 + (pos * spd * facter2)));
+            adjy += int(scale * (pos/decay) * cos(starter2 + (pos * spd * facter2)));
 
-        // Get the values to the servos.
-        servox.write(adjx);
-        servoy.write(adjy);
-        delay(1); // Just for consistent speed.
+            if (counter == 0)
+            {
+                max_dev = max(max_dev, abs(adjx - 90));
+                max_dev = max(max_dev, abs(adjy - 90));
+            }
+            if (counter == 1)
+            {
+                // Get the values to the servos.
+                servox.write(adjx);
+                servoy.write(adjy);
+                delay(1); // Just for consistent speed.
+            }
+        } 
+    counter += 1;
+    scale = int(scale*scale/max_dev);
     } 
 
     // Return to center after drawing.
