822a4d2c85aa1a3b0e3480348ffc90225a72b431
Johan Beyers <johan@juizi.com>
2017-11-25 18:06:05 +0200

    Add ipywidgets

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ virtualenv ve --python=python3.6
 . ve/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt 
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+deactivate
